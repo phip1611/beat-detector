1080c75b9acb83849e1822efc91404a35b4f78c6
Philipp Schuster <phip1611@gmail.com>
2024-05-04 17:25:55 +0200

    nix: update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell rec {
   packages = with pkgs; [
+    # Base deps
     alsa-lib
     pkg-config
+
+    # gui examples (minifb)
+    libxkbcommon
+    xorg.libXcursor
+    xorg.libX11
+
+    # Development
+    nixpkgs-fmt
+    rustup
   ];
 }
